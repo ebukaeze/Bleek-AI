c6f2e19c7ee0912460adb0aa1e8c5c12b1a09869
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-04-19 00:07:56 +0100

    chore: added sign in form with usestate to set form data and react context for global store

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,26 +1,49 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
-import { Link } from "expo-router";
+import { Link, Redirect, router } from "expo-router";
 import { images } from "../../constants/constants";
 import FormField from "../components/FormField";
 import CustomButton from "../components/CustomButton";
-
-const submit = () => {};
+import { appwriteConfig, getCurrentUser, signIn } from "../../lib/appwrite";
+import { useAuthContext } from "../../context/AuthContext";
 
 const SignIn = () => {
+  const { setUser, setIsLoggedIn } = useAuthContext();
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
   const [isSubmtting, setIsSubmtting] = useState(false);
+  const { email, password } = form;
+
+  const submit = async () => {
+    if (!email || !password) {
+      Alert.alert("Empty Field", "please fill in all the fileds");
+    }
+    setIsSubmtting(true);
+
+    try {
+      await signIn(email, password);
+      const result = getCurrentUser();
+
+      //set user to global state
+      setUser(result);
+      setIsLoggedIn(true);
+      Alert.alert("Success", "user signed in successfully");
+      +router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmtting(false);
+    }
+  };
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView style={{ height: "100%" }}>
         <View className="w-full items-center justify-center h-full py-8 px-4">
-          <Text className="text-4xl text-secondary-200 pt-4 font-bold">
-            Bleek
-          </Text>
+          <Text className="text-4xl text-secondary pt-4 font-bold">Bleek</Text>
           <Text className="text-xl text-white mt-4">Sign In to Bleek</Text>
           <View className="mt-4 w-full">
             <FormField
@@ -46,7 +69,7 @@ const SignIn = () => {
           <View className="w-full gap-2 justify-center pt-5 flex-row">
             <Text className="text-base text-white">
               Don't have an account ?{" "}
-              <Text className="text-secondary-100">
+              <Text className="text-secondary">
                 <Link href="/sign-up">Sign Up</Link>
               </Text>
             </Text>
